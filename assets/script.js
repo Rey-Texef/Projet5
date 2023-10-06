f63c529c4dc3a820c383395608cbba3047e43a30
Rey-Texef <vip_rf@hotmail.com>
2023-10-06 02:36:56 +0200

    mise en place des flèches et dots

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,25 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let flecheGauche = document.getElementById("arrow_left")
+flecheGauche.addEventListener("click", function () {
+	console.log("Vous avez cliqué sur la flèche gauche")
+})
+
+let flecheDroite = document.getElementById("arrow_right")
+flecheDroite.addEventListener("click", function () {
+	rightArrow(i)
+	console.log("Vous avez cliqué sur la flèche droite")
+})
+
+let i=0
+
+function rightArrow(i) {
+	let actualDot = document.querySelector(".dot_selected")
+	actualDot.classList.remove("dot_selected")
+	i+=1
+	let text = document.querySelector("#banner p")
+	text.innerHTML = slides[i].tagLine
+	return i
+}
